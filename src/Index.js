ac8df5a9b06b320af3171f9e728b15bc546cbd1d
Amine Benseddik <amine.benseddik@gmail.com>
2015-12-31 15:40:53 +0100

    Adding hong kong

--- src/Index.js
+++ src/Index.js
@@ -6,10 +6,11 @@ window.React = React;
 
 var clocks = [
   { id: 1, town: 'New York', timezone: 'America/New_York' },
-  { id: 2, town: 'Casablanca', timezone: 'Africa/Casablanca', locale: 'ar' },
+  { id: 2, town: 'الدار البيضاء', timezone: 'Africa/Casablanca', locale: 'fr' },
   { id: 3, town: 'Paris', timezone: 'Europe/Paris', locale: 'fr' },
-  { id: 4, town: 'New Dehli', timezone: 'Asia/Kolkata', locale: 'hi' },
-  { id: 5, town: 'Tokyo', timezone: 'Asia/Tokyo', locale: 'ja' },
+  { id: 4, town: '香港', timezone: 'Asia/Hong_Kong', locale: 'zh-cn' },
+  { id: 5, town: 'नई दिल्ली', timezone: 'Asia/Kolkata', locale: 'hi' },
+  { id: 6, town: '東京', timezone: 'Asia/Tokyo', locale: 'ja' },
 ];
 
 ReactDOM.render(
